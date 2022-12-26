a4ebd1b3015f6cfdf325e6e141e9940c2defa482
yosephtesfaye <tewdemam@gmail.com>
2022-12-26 03:51:38 -0800

    modifed

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -18,52 +18,50 @@ from sqlalchemy.sql import exists
 
 if __name__ == "__main__":
 
-        """ Engine connection
+    """ Engine connection
 
-            """
+    """
 
-                engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
 
-                                                   .format(sys.argv[1], sys.argv[2],
+                           .format(sys.argv[1], sys.argv[2],
 
-                                                                                          sys.argv[3]), pool_pre_ping=True)
+                                   sys.argv[3]), pool_pre_ping=True)
 
-                                                       Base.metadata.create_all(engine)
-
-
-
-                                                           """ Session handling
-
-                                                               """
+    Base.metadata.create_all(engine)
 
 
 
-                                                                   Session = sessionmaker(bind=engine)
+    """ Session handling
 
-                                                                       session = Session()
-
-
-
-                                                                           """ Query
-
-                                                                               """
-
-                                                                                   if "'" not in sys.argv[4]:
-
-                                                                                               check = session.query(State).filter_by(name=sys.argv[4]).scalar()
-
-                                                                                                       if check is None:
-
-                                                                                                                       print("Not found")
-
-                                                                                                                               else:
-
-                                                                                                                                               query = session.query(State).filter_by(name=sys.argv[4])
-
-                                                                                                                                                           for row in query.all():
-
-                                                                                                                                                                               print("{}".format(row.id))
+    """
 
 
 
-                                                                                                                                                                                   session.close()
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+
+
+    """ Query
+
+    """
+
+    if "'" not in sys.argv[4]:
+
+        check = session.query(State).filter_by(name=sys.argv[4]).scalar()
+
+        if check is None:
+
+            print("Not found")
+
+        else:
+
+            query = session.query(State).filter_by(name=sys.argv[4])
+
+            for row in query.all():
+
+                print("{}".format(row.id))
+
+                session.close()
