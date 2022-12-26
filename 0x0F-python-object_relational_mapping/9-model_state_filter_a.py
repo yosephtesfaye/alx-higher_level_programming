92d0464df0643e245fc1051a84c23aeaf928f6cb
yosephtesfaye <tewdemam@gmail.com>
2022-12-26 03:47:38 -0800

    modifed

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -16,46 +16,43 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
 
-        """ Engine connection
+    """ Engine connection
 
-            """
+    """
 
-                engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
 
-                                                   .format(sys.argv[1], sys.argv[2],
+                           .format(sys.argv[1], sys.argv[2],
 
-                                                                                          sys.argv[3]), pool_pre_ping=True)
+                                   sys.argv[3]), pool_pre_ping=True)
 
-                                                       Base.metadata.create_all(engine)
-
-
-
-                                                           """ Session handling
-
-                                                               """
+    Base.metadata.create_all(engine)
 
 
 
-                                                                   Session = sessionmaker(bind=engine)
+    """ Session handling
 
-                                                                       session = Session()
-
-
-
-                                                                           """ Query
-
-                                                                               """
+    """
 
 
 
-                                                                                   for state in session.query(State)\
+    Session = sessionmaker(bind=engine)
 
-                                                                                                                   .order_by(State.id)\
-
-                                                                                                                                                   .filter(State.name.like('%a%')):
-
-                                                                                                                                                               print("{}: {}".format(state.id, state.name))
+    session = Session()
 
 
 
-                                                                                                                                                                       session.close()
+    """ Query
+
+    """
+
+
+
+    for state in session.query(State)\
+
+                        .order_by(State.id)\
+
+                        .filter(State.name.like('%a%')):
+
+        print("{}: {}".format(state.id, state.name))
+        session.close()
