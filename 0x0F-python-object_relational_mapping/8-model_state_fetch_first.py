e17c6a58e24e80e4d4c2301bcd7624bcadc1af9f
yosephtesfaye <tewdemam@gmail.com>
2022-12-26 03:43:50 -0800

    modifed

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -18,46 +18,44 @@ from sqlalchemy.orm.exc import NoResultFound
 
 if __name__ == "__main__":
 
-        """ Engine connection
+    """ Engine connection
 
-            """
+    """
 
-                engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
 
-                                                   .format(sys.argv[1], sys.argv[2],
+                           .format(sys.argv[1], sys.argv[2],
 
-                                                                                          sys.argv[3]), pool_pre_ping=True)
+                                   sys.argv[3]), pool_pre_ping=True)
 
-                                                       Base.metadata.create_all(engine)
-
-
-
-                                                           """ Session handling
-
-                                                               """
+    Base.metadata.create_all(engine)
 
 
 
-                                                                   Session = sessionmaker(bind=engine)
+    """ Session handling
 
-                                                                       session = Session()
-
-
-
-                                                                           """ Query
-
-                                                                               """
-
-                                                                                   query = session.query(State).order_by(State.id).first()
-
-                                                                                       if query:
-
-                                                                                                   print("{}: {}".format(query.id, query.name))
-
-                                                                                                       else:
-
-                                                                                                                   print("Nothing")
+    """
 
 
 
-                                                                                                                       session.close()
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+
+
+    """ Query
+
+    """
+
+    query = session.query(State).order_by(State.id).first()
+
+    if query:
+
+        print("{}: {}".format(query.id, query.name))
+
+    else:
+
+        print("Nothing")
+        session.close()
+
