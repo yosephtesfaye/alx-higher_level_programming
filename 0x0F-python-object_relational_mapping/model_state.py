cb1f6d1637f8f0bfeb4e2dc334aaf7767015e565
yosephtesfaye <tewdemam@gmail.com>
2022-12-26 03:37:20 -0800

    modified

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -24,12 +24,11 @@ Base = declarative_base()
 
 class State(Base):
 
-        """ State class inherits Base sqlalchemy call
+    """ State class inherits Base sqlalchemy call
 
-            """
+    """
 
-                __tablename__ = 'states'
+    __tablename__ = 'states'
 
-                    id = Column(Integer, primary_key=True)
-
-                        name = Column(String(128))
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128))
